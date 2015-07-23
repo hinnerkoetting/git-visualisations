a7d378362852d5fbac890653327cecb4662d1259
Hinnerk Oetting <hinnerk.oetting@gmail.com>
2015-07-23 08:48:07 +0200

    Send files from public folder

--- server.js
+++ server.js
@@ -4,15 +4,32 @@ var http = require('http');
 var express = require('express');
 var app = express();
 var git = require('./services/git.js');
+var send = require('send');
+var parseUrl = require('url').parse;
 
 configureRoutes();
 startServer();
 
 
 function configureRoutes() {
+  app.get('/', fromFileSystem('./public/index.html'));
   app.get('/git', git);
-
+  app.get('/*', fileFromPublicFolder);
 }
+
+
+
+function fileFromPublicFolder(request, response) {
+  var url = parseUrl(request.url, true);  
+  fromFileSystem( 'public' + url.pathname )(request, response);
+}
+
+function fromFileSystem(path) {
+  return function(request, response) {
+     send(request, path).pipe(response);
+  }
+}
+
 
 function startServer() {
   var server = app.listen(3000, function () {
